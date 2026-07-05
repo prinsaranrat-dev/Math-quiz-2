4654b3d039020440fb313d236acea3c148a232dc
prinsaranrat-dev <prinsaranrat@gmail.com>
2026-07-05 21:12:25 +0700

    main.py

--- main.py
+++ main.py
@@ -2,30 +2,30 @@ import random
 Question = 1
 Score = 0
 while Question <= 10:
-    apple1 = random.randint(1,12)
-    apple2 = random.randint(1,12)
+    pizza1 = random.randint(1,12)
+    pizza2 = random.randint(1,12)
     print("Question",Question)
     if Question % 2 == 0:
-        if apple1 < apple2:
-            apple1, apple2 = apple2, apple1
-        print(f"{apple1} 🍎 × {apple2} 🍎 = ?")
+        if pizza1 < pizza2:
+            pizza1, pizza2 = pizza2, pizza1
+        print(f"{pizza1} 🍕 × {pizza2} 🍕 = ?")
     else:
-         print(f"{apple1} 🍎 ÷ {apple2} 🍎 = ?")
+         print(f"{pizza1} 🍕 ÷ {pizza2} 🍕 = ?")
     ans = int(input(""))
     if Question % 2 == 0:
-        if ans == apple1 * apple2:
+        if ans == pizza1 * pizza2:
             print("Correct!")
             Score += 1
         else:
             print("Incorrect!")
-            print("The answer is", apple1 * apple2)
+            print("The answer is", pizza1 *pizza2)
     else:
-          if ans == apple1 / apple2:
+          if ans == pizza1 / pizza2:
               print("Correct!")
               Score += 1
           else:
               print("Incorrect!")
-              print("The answer is", apple1 / apple2)
+              print("The answer is", pizza1 / pizza2)
     Question += 1
             
 print("================")
